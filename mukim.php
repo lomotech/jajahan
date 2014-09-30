4b7372e0d7844978f348fb81806d805a61173996
syafiq <syafiq@my-sands.com>
2014-09-30 12:50:50 +0800

    update mukim johor

---
+++ mukim.php
@@ -0,0 +1,203 @@
+<?php 
+
+$items = [
+    /*
+     * perlis
+     */
+    
+    /*
+     * kedah
+     */
+
+    /*
+     * kelantan
+     */
+    
+    /*
+     * terengganu
+     */
+    
+    /*
+     * pulau pinang
+     */
+    
+    /*
+     * perak
+     */
+    
+    /*
+     * pahang
+     */
+    
+    /*
+     * selangor
+     */
+    
+    /*
+     * kuala lumpur
+     */
+    
+    /*
+     * putrajaya
+     */
+    
+    /*
+     * negeri sembilan
+     */
+    
+    /*
+     * melaka
+     */
+    
+    /*
+     * johor
+     */
+    /*
+     * === batu pahat
+     */
+    ['id' => '010101', 'name' => 'BAGAN', 'daerah_id' => '0101'],
+    ['id' => '010102', 'name' => 'CHAAH BAHRU', 'daerah_id' => '0101'],
+    ['id' => '010103', 'name' => 'KAMPONG BAHRU', 'daerah_id' => '0101'],
+    ['id' => '010104', 'name' => 'LINAU', 'daerah_id' => '0101'],
+    ['id' => '010105', 'name' => 'LUBOK', 'daerah_id' => '0101'],
+    ['id' => '010106', 'name' => 'MINYAK BEKU', 'daerah_id' => '0101'],
+    ['id' => '010107', 'name' => 'PESERAI', 'daerah_id' => '0101'],
+    ['id' => '010108', 'name' => 'SIMPANG KANAN', 'daerah_id' => '0101'],
+    ['id' => '010109', 'name' => 'SIMPANG KIRI', 'daerah_id' => '0101'],
+    ['id' => '010110', 'name' => 'SRI GADING', 'daerah_id' => '0101'],
+    ['id' => '010111', 'name' => 'SRI MEDAN', 'daerah_id' => '0101'],
+    ['id' => '010112', 'name' => 'SUNGAI KLUANG', 'daerah_id' => '0101'],
+    ['id' => '010113', 'name' => 'SUNGAI PUNGGOR', 'daerah_id' => '0101'],
+    ['id' => '010114', 'name' => 'TANJONG SEMBRONG', 'daerah_id' => '0101'],
+    /*
+     * === johor bahru
+     */
+    ['id' => '010201', 'name' => 'PLENTONG', 'daerah_id' => '0102'],
+    ['id' => '010202', 'name' => 'PULAI', 'daerah_id' => '0102'],
+    // ['id' => '010203', 'name' => 'SEDENAK', 'daerah_id' => '0102'], //change to kulaijaya since 2008
+    // ['id' => '010204', 'name' => 'SENAI / KULAI', 'daerah_id' => '0102'], //change to kulaijaya since 2008
+    ['id' => '010205', 'name' => 'SUNGAI TIRAM', 'daerah_id' => '0102'],
+    ['id' => '010206', 'name' => 'TANJUNG KUPANG', 'daerah_id' => '0102'],
+    ['id' => '010207', 'name' => 'TEMBRAU', 'daerah_id' => '0102'],
+    ['id' => '010208', 'name' => 'BANDAR JOHOR BAHRU', 'daerah_id' => '0102'],
+    ['id' => '010209', 'name' => 'JELUTONG', 'daerah_id' => '0102'],
+    /*
+     * === kluang
+     */
+    ['id' => '010301', 'name' => 'KAHANG', 'daerah_id' => '0103'],
+    ['id' => '010302', 'name' => 'KLUANG', 'daerah_id' => '0103'],
+    ['id' => '010303', 'name' => 'LAYANG-LAYANG', 'daerah_id' => '0103'],
+    ['id' => '010304', 'name' => 'MACHAP', 'daerah_id' => '0103'],
+    ['id' => '010305', 'name' => 'NIYOR', 'daerah_id' => '0103'],
+    ['id' => '010306', 'name' => 'PALOH', 'daerah_id' => '0103'],
+    ['id' => '010307', 'name' => 'RENGAM', 'daerah_id' => '0103'],
+    ['id' => '010308', 'name' => 'ULU BENUT', 'daerah_id' => '0103'],
+    /*
+     * === kota tinggi
+     */
+    ['id' => '010401', 'name' => 'JOHOR LAMA', 'daerah_id' => '0104'],
+    ['id' => '010402', 'name' => 'KAMBAU', 'daerah_id' => '0104'],
+    ['id' => '010403', 'name' => 'KOTA TINGGI', 'daerah_id' => '0104'],
+    ['id' => '010404', 'name' => 'PANTAI TIMUR', 'daerah_id' => '0104'],
+    ['id' => '010405', 'name' => 'PENGGERANG', 'daerah_id' => '0104'],
+    ['id' => '010406', 'name' => 'SELIDI BESAR', 'daerah_id' => '0104'],
+    ['id' => '010407', 'name' => 'SELIDI KECHIL', 'daerah_id' => '0104'],
+    ['id' => '010408', 'name' => 'TANJUNG SURAT', 'daerah_id' => '0104'],
+    ['id' => '010409', 'name' => 'ULU SUNGAI JOHOR', 'daerah_id' => '0104'],
+    ['id' => '010410', 'name' => 'ULU SUNGAI SELIDI BESAR', 'daerah_id' => '0104'],
+    /*
+     * === mersing
+     */
+    ['id' => '010501', 'name' => 'JEMALUANG', 'daerah_id' => '0105'],
+    ['id' => '010502', 'name' => 'LENGGOR', 'daerah_id' => '0105'],
+    ['id' => '010503', 'name' => 'MERSING', 'daerah_id' => '0105'],
+    ['id' => '010504', 'name' => 'PADANG ENDAU', 'daerah_id' => '0105'],
+    ['id' => '010505', 'name' => 'PENYABONG', 'daerah_id' => '0105'],
+    ['id' => '010506', 'name' => 'PULAU AUR', 'daerah_id' => '0105'],
+    ['id' => '010507', 'name' => 'PULAU BABI', 'daerah_id' => '0105'],
+    ['id' => '010508', 'name' => 'PULAU PEMANGGIL', 'daerah_id' => '0105'],
+    ['id' => '010509', 'name' => 'PULAU SIBU', 'daerah_id' => '0105'],
+    ['id' => '010510', 'name' => 'PULAU TINGGI', 'daerah_id' => '0105'],
+    ['id' => '010511', 'name' => 'SEMBRONG', 'daerah_id' => '0105'],
+    ['id' => '010512', 'name' => 'TENGGAROH', 'daerah_id' => '0105'],
+    ['id' => '010513', 'name' => 'TENGLU', 'daerah_id' => '0105'],
+    ['id' => '010514', 'name' => 'TRIANG', 'daerah_id' => '0105'],
+    /*
+     * === muar
+     */
+    ['id' => '010501', 'name' => 'AYER HITAM', 'daerah_id' => '0106'],
+    ['id' => '010602', 'name' => 'BANDAR', 'daerah_id' => '0106'],
+    ['id' => '010603', 'name' => 'BUKIT KEPONG', 'daerah_id' => '0106'],
+    // ['id' => '010604', 'name' => 'BUKIT SERAMPANG', 'daerah_id' => '0106'], //tukar ke ledang since 2008
+    // ['id' => '010605', 'name' => 'GRISEK', 'daerah_id' => '0106'], //tukar ke ledang since 2008
+    ['id' => '010606', 'name' => 'JALAN BAKRI', 'daerah_id' => '0106'],
+    ['id' => '010607', 'name' => 'JORAK', 'daerah_id' => '0106'],
+    // ['id' => '010608', 'name' => 'KESANG', 'daerah_id' => '0106'], //tukar ke ledang since 2008
+    // ['id' => '010609', 'name' => 'KUNDANG', 'daerah_id' => '0106'], //tukar ke ledang since 2008
+    ['id' => '010610', 'name' => 'LENGA', 'daerah_id' => '0106'],
+    ['id' => '010611', 'name' => 'PARIT BAKAR', 'daerah_id' => '0106'],
+    ['id' => '010612', 'name' => 'PARIT JAWA', 'daerah_id' => '0106'],
+    ['id' => '010613', 'name' => 'SEROM', 'daerah_id' => '0106'],
+    ['id' => '010614', 'name' => 'SRI MERANTI', 'daerah_id' => '0106'],
+    ['id' => '010615', 'name' => 'SUNGAI BALANG', 'daerah_id' => '0106'],
+    ['id' => '010616', 'name' => 'SUNGAI RAYA & KG. BUKIT PASIR', 'daerah_id' => '0106'],
+    ['id' => '010617', 'name' => 'SUNGAI TERAP', 'daerah_id' => '0106'],
+    // ['id' => '010618', 'name' => 'TANGKAK', 'daerah_id' => '0106'], //tukar ke ledang since 2008
+    /*
+     * === pontian
+     */
+    ['id' => '010701', 'name' => 'API-API', 'daerah_id' => '0107'],
+    ['id' => '010702', 'name' => 'AYER BALOI', 'daerah_id' => '0107'],
+    ['id' => '010703', 'name' => 'AIR MASIN', 'daerah_id' => '0107'],
+    ['id' => '010704', 'name' => 'BENUT', 'daerah_id' => '0107'],
+    ['id' => '010705', 'name' => 'JERAM BATU', 'daerah_id' => '0107'],
+    ['id' => '010706', 'name' => 'PONTIAN', 'daerah_id' => '0107'],
+    ['id' => '010707', 'name' => 'RIMBA TERJUN', 'daerah_id' => '0107'],
+    ['id' => '010708', 'name' => 'SERKAT', 'daerah_id' => '0107'],
+    ['id' => '010709', 'name' => 'SUNGAI KARANG', 'daerah_id' => '0107'],
+    ['id' => '010710', 'name' => 'SUNGEI PINGGAN', 'daerah_id' => '0107'],
+    ['id' => '010711', 'name' => 'PENGKALAN RAJA', 'daerah_id' => '0107'],
+    /*
+     * === segamat
+     */
+    ['id' => '010801', 'name' => 'BEKOK', 'daerah_id' => '0108'],
+    ['id' => '010802', 'name' => 'BULOH KASAP', 'daerah_id' => '0108'],
+    ['id' => '010803', 'name' => 'CHAAH', 'daerah_id' => '0108'],
+    ['id' => '010804', 'name' => 'GEMAS', 'daerah_id' => '0108'],
+    ['id' => '010805', 'name' => 'GEMEREH', 'daerah_id' => '0108'],
+    ['id' => '010806', 'name' => 'JABI', 'daerah_id' => '0108'],
+    ['id' => '010807', 'name' => 'JEMENTAH', 'daerah_id' => '0108'],
+    ['id' => '010808', 'name' => 'LABIS', 'daerah_id' => '0108'],
+    ['id' => '010809', 'name' => 'POGOH', 'daerah_id' => '0108'],
+    ['id' => '010810', 'name' => 'SERMIN', 'daerah_id' => '0108'],
+    ['id' => '010811', 'name' => 'SUNGAI SEGAMAT', 'daerah_id' => '0108'],
+    ['id' => '010812', 'name' => 'BANDAR SEGAMAT', 'daerah_id' => '0108'],
+    /*
+     * === kulaijaya // upgrade from kulai since 2008 - http://www.utusan.com.my/utusan/info.asp?y=2008&dt=0622&pub=Utusan_Malaysia&sec=Johor&pg=wj_01.htm
+     */
+    ['id' => '010901', 'name' => 'SENAI', 'daerah_id' => '0109'],
+    ['id' => '010902', 'name' => 'KULAI', 'daerah_id' => '0109'],
+    ['id' => '010903', 'name' => 'BUKIT BATU', 'daerah_id' => '0109'],
+    ['id' => '010904', 'name' => 'SEDENAK', 'daerah_id' => '0109'],
+    /*
+     * === ledang // upgrade since 2008
+     */
+    ['id' => '011001', 'name' => 'TANGKAK', 'daerah_id' => '0110'],
+    ['id' => '011002', 'name' => 'SEROM', 'daerah_id' => '0110'],
+    ['id' => '011003', 'name' => 'GERSIK', 'daerah_id' => '0110'],
+    ['id' => '011004', 'name' => 'BUKIT SERAMPANG', 'daerah_id' => '0110'],
+    ['id' => '011005', 'name' => 'KUNDANG', 'daerah_id' => '0110'],
+    ['id' => '011006', 'name' => 'KESANG', 'daerah_id' => '0110'],
+    
+    /*
+     * labuan
+     */
+    
+    /*
+     * sabah
+     */
+    
+    /*
+     * sarawak
+     */
+];
