d4e82d4b0509fc84daf708899c490cae577e3d61
syafiq <syafiq@my-sands.com>
2014-09-30 12:17:10 +0800

    add district, parliament, state

---
+++ district.php
@@ -0,0 +1,178 @@
+<?php 
+
+$items = [
+    /*
+     * perlis
+     */
+    ['id' => '0901', 'state_id' = '9', 'name' => 'PERLIS', 'code_3' => 'PER'],
+    /*
+     * kedah
+     */
+    ['id' => '0201', 'state_id' = '2', 'name' => 'KOTA SETAR', 'code_3' => 'KSE'],
+    ['id' => '0202', 'state_id' = '2', 'name' => 'KUBANG PASU', 'code_3' => 'KPA'],
+    ['id' => '0203', 'state_id' = '2', 'name' => 'PADANG TERAP', 'code_3' => 'PTE'],
+    ['id' => '0204', 'state_id' = '2', 'name' => 'LANGKAWI', 'code_3' => 'LAN'],
+    ['id' => '0205', 'state_id' = '2', 'name' => 'KUALA MUDA', 'code_3' => 'KMU'],
+    ['id' => '0206', 'state_id' = '2', 'name' => 'YAN', 'code_3' => 'YAN'],
+    ['id' => '0207', 'state_id' = '2', 'name' => 'SIK', 'code_3' => 'SIK'],
+    ['id' => '0208', 'state_id' = '2', 'name' => 'BALING', 'code_3' => 'BAL'],
+    ['id' => '0209', 'state_id' = '2', 'name' => 'KULIM', 'code_3' => 'KUL'],
+    ['id' => '0210', 'state_id' = '2', 'name' => 'BANDAR BAHARU', 'code_3' => 'BBA'],
+    ['id' => '0211', 'state_id' = '2', 'name' => 'PENDANG', 'code_3' => 'PEN'],
+    ['id' => '0212', 'state_id' = '2', 'name' => 'POKOK SENA', 'code_3' => 'PSE'],
+    /*
+     * kelantan
+     */
+    ['id' => '0301', 'state_id' = '3', 'name' => 'BACHOK', 'code_3' => 'BAC'],
+    ['id' => '0302', 'state_id' = '3', 'name' => 'KOTA BHARU', 'code_3' => 'KBH'],
+    ['id' => '0303', 'state_id' = '3', 'name' => 'MACHANG', 'code_3' => 'MAC'],
+    ['id' => '0304', 'state_id' = '3', 'name' => 'PASIR MAS', 'code_3' => 'PMA'],
+    ['id' => '0305', 'state_id' = '3', 'name' => 'PASIR PUTEH', 'code_3' => 'PPU'],
+    ['id' => '0306', 'state_id' = '3', 'name' => 'TANAH MERAH', 'code_3' => 'TME'],
+    ['id' => '0307', 'state_id' = '3', 'name' => 'TUMPAT', 'code_3' => 'TUM'],
+    ['id' => '0308', 'state_id' = '3', 'name' => 'GUA MUSANG', 'code_3' => 'GMU'],
+    // ['id' => '0309', 'state_id' = '3', 'name' => '??'],
+    ['id' => '0310', 'state_id' = '3', 'name' => 'KUALA KRAI', 'code_3' => 'KKR'],
+    ['id' => '0311', 'state_id' = '3', 'name' => 'JELI', 'code_3' => 'JEL'],
+
+    /*
+     * terengganu
+     */
+    ['id' => '1101', 'state_id' = '11', 'name' => 'BESUT', 'code_3' => 'BES'],
+    ['id' => '1102', 'state_id' = '11', 'name' => 'DUNGUN', 'code_3' => 'DUN'],
+    ['id' => '1103', 'state_id' = '11', 'name' => 'KEMAMAN', 'code_3' => 'KEM'],
+    ['id' => '1104', 'state_id' = '11', 'name' => 'KUALA TERENGGANU', 'code_3' => 'KTE'],
+    ['id' => '1105', 'state_id' = '11', 'name' => 'HULU TERENGGANU', 'code_3' => 'HTE'],
+    ['id' => '1106', 'state_id' = '11', 'name' => 'MARANG', 'code_3' => 'MAR'],
+    ['id' => '1107', 'state_id' = '11', 'name' => 'SETIU', 'code_3' => 'SET'],
+    /*
+     * pulau pinang
+     */
+    ['id' => '0701', 'state_id' = '7', 'name' => 'SEBERANG PERAI TENGAH', 'code_3' => 'SPT'],
+    ['id' => '0702', 'state_id' = '7', 'name' => 'SEBERANG PERAI UTARA', 'code_3' => 'SPU'],
+    ['id' => '0703', 'state_id' = '7', 'name' => 'SEBERANG PERAI SELATAN', 'code_3' => 'SPS'],
+    ['id' => '0704', 'state_id' = '7', 'name' => 'TIMOR LAUT', 'code_3' => 'TLA'],
+    ['id' => '0705', 'state_id' = '7', 'name' => 'BARAT DAYA', 'code_3' => 'BDA'],
+    /*
+     * perak
+     */
+    ['id' => '0801', 'state_id' = '8', 'name' => 'BATANG PADANG', 'code_3' => 'BPA'],
+    ['id' => '0802', 'state_id' = '8', 'name' => 'MANJUNG', 'code_3' => 'MAN'],
+    ['id' => '0803', 'state_id' = '8', 'name' => 'KINTA', 'code_3' => 'KIN'],
+    ['id' => '0804', 'state_id' = '8', 'name' => 'KERIAN', 'code_3' => 'KER'],
+    ['id' => '0805', 'state_id' = '8', 'name' => 'KUALA KANGSAR', 'code_3' => 'KKA'],
+    ['id' => '0806', 'state_id' = '8', 'name' => 'LARUT & MATANG', 'code_3' => 'LDM'],
+    ['id' => '0807', 'state_id' = '8', 'name' => 'HILIR PERAK', 'code_3' => 'HPE'],
+    ['id' => '0808', 'state_id' = '8', 'name' => 'HULU PERAK', 'code_3' => 'HPR'],
+    ['id' => '0809', 'state_id' = '8', 'name' => 'SELAMA', 'code_3' => 'SEL'],
+    ['id' => '0810', 'state_id' = '8', 'name' => 'PERAK TENGAH', 'code_3' => 'PTE'],
+    ['id' => '0811', 'state_id' = '8', 'name' => 'KAMPAR', 'code_3' => 'KAM'],
+    /*
+     * pahang
+     */
+    ['id' => '0601', 'state_id' = '6', 'name' => 'BENTONG', 'code_3' => 'BEN'],
+    ['id' => '0602', 'state_id' = '6', 'name' => 'CAMERON HIGHLANDS', 'code_3' => 'CHI'],
+    ['id' => '0603', 'state_id' = '6', 'name' => 'JERANTUT', 'code_3' => 'JER'],
+    ['id' => '0604', 'state_id' = '6', 'name' => 'KUANTAN', 'code_3' => 'KUA'],
+    ['id' => '0605', 'state_id' = '6', 'name' => 'LIPIS', 'code_3' => 'LIP'],
+    ['id' => '0606', 'state_id' = '6', 'name' => 'PEKAN', 'code_3' => 'PEK'],
+    ['id' => '0607', 'state_id' = '6', 'name' => 'RAUB', 'code_3' => 'RAU'],
+    ['id' => '0608', 'state_id' = '6', 'name' => 'TEMERLOH', 'code_3' => 'TER'],
+    ['id' => '0609', 'state_id' = '6', 'name' => 'ROMPIN', 'code_3' => 'ROM'],
+    ['id' => '0610', 'state_id' = '6', 'name' => 'MARAN', 'code_3' => 'MAR'],
+    ['id' => '0611', 'state_id' = '6', 'name' => 'BERA', 'code_3' => 'BER'],
+    /*
+     * selangor
+     */
+    ['id' => '0601', 'state_id' = '10', 'name' => 'KLANG', 'code_3' => 'KLA'],
+    ['id' => '0602', 'state_id' = '10', 'name' => 'KUALA LANGAT', 'code_3' => 'KLN'],
+    ['id' => '0603', 'state_id' = '10', 'name' => 'KUALA SELANGOR', 'code_3' => 'KSE'],
+    ['id' => '0604', 'state_id' = '10', 'name' => 'SABAK BERNAM', 'code_3' => 'SBE'],
+    ['id' => '0605', 'state_id' = '10', 'name' => 'ULU LANGAT', 'code_3' => 'ULA'],
+    ['id' => '0606', 'state_id' = '10', 'name' => 'ULU SELANGOR', 'code_3' => 'USE'],
+    ['id' => '0607', 'state_id' = '10', 'name' => 'PETALING', 'code_3' => 'PET'],
+    ['id' => '0608', 'state_id' = '10', 'name' => 'GOMBAK', 'code_3' => 'GOM'],
+    ['id' => '0609', 'state_id' = '10', 'name' => 'SEPANG', 'code_3' => 'SEP'],
+    /*
+     * kuala lumpur
+     */
+    ['id' => '1401', 'state_id' = '14', 'name' => 'W. P. KUALA LUMPUR', 'code_3' => 'WPK'],
+    /*
+     * putrajaya
+     */
+    ['id' => '1601', 'state_id' = '16', 'name' => 'W. P. PUTRAJAYA', 'code_3' => 'WPP'],
+    /*
+     * negeri sembilan
+     */
+    ['id' => '0501', 'state_id' = '4', 'name' => 'JELEBU', 'code_3' => 'JEL'],
+    ['id' => '0502', 'state_id' = '4', 'name' => 'KUALA PILAH', 'code_3' => 'KPI'],
+    ['id' => '0503', 'state_id' = '4', 'name' => 'PORT DICKSON', 'code_3' => 'PDI'],
+    ['id' => '0504', 'state_id' = '4', 'name' => 'REMBAU', 'code_3' => 'REM'],
+    ['id' => '0505', 'state_id' = '4', 'name' => 'SEREMBAN', 'code_3' => 'SER'],
+    ['id' => '0506', 'state_id' = '4', 'name' => 'TAMPIN', 'code_3' => 'TAM'],
+    ['id' => '0507', 'state_id' = '4', 'name' => 'JEMPOL', 'code_3' => 'JEM'],
+    /*
+     * melaka
+     */
+    ['id' => '0401', 'state_id' = '4', 'name' => 'MELAKA TENGAH', 'code_3' => 'MTE'],
+    ['id' => '0402', 'state_id' = '4', 'name' => 'JASIN', 'code_3' => 'JAS'],
+    ['id' => '0403', 'state_id' = '4', 'name' => 'ALOR GAJAH', 'code_3' => 'AGA'],
+    /*
+     * johor
+     */
+    ['id' => '0101', 'state_id' = '1', 'name' => 'BATU PAHAT', 'code_3' => 'BPA'],
+    ['id' => '0102', 'state_id' = '1', 'name' => 'JOHOR BAHRU', 'code_3' => 'JBA'],
+    ['id' => '0103', 'state_id' = '1', 'name' => 'KLUANG', 'code_3' => 'KLU'],
+    ['id' => '0104', 'state_id' = '1', 'name' => 'KOTA TINGGI', 'code_3' => 'KTI'],
+    ['id' => '0105', 'state_id' = '1', 'name' => 'MERSING', 'code_3' => 'MER'],
+    ['id' => '0106', 'state_id' = '1', 'name' => 'MUAR', 'code_3' => 'MUA'],
+    ['id' => '0107', 'state_id' = '1', 'name' => 'PONTIAN', 'code_3' => 'PON'],
+    ['id' => '0108', 'state_id' = '1', 'name' => 'SEGAMAT', 'code_3' => 'SEG'],
+    ['id' => '0109', 'state_id' = '1', 'name' => 'KULAIJAYA', 'code_3' => 'KUL'],
+    ['id' => '0110', 'state_id' = '1', 'name' => 'LEDANG', 'code_3' => 'LED'],
+    /*
+     * labuan
+     */
+    ['id' => '1501', 'state_id' = '15', 'name' => 'W. P. LABUAN', 'code_3' => 'WPL'],
+    /*
+     * sabah
+     */
+    ['id' => '1201', 'state_id' = '12', 'name' => 'KOTA KINABALU', 'code_3' => 'KKI'],
+    ['id' => '1202', 'state_id' = '12', 'name' => 'PAPAR', 'code_3' => 'PAP'],
+    ['id' => '1203', 'state_id' = '12', 'name' => 'KOTA BELUD', 'code_3' => 'KBE'],
+    ['id' => '1204', 'state_id' = '12', 'name' => 'TUARAN', 'code_3' => 'TUA'],
+    ['id' => '1205', 'state_id' = '12', 'name' => 'KUDAT', 'code_3' => 'KUD'],
+    ['id' => '1206', 'state_id' = '12', 'name' => 'RANAU', 'code_3' => 'RAN'],
+    ['id' => '1207', 'state_id' = '12', 'name' => 'SANDAKAN', 'code_3' => 'SAN'],
+    ['id' => '1208', 'state_id' = '12', 'name' => 'LABUK & SUGUT', 'code_3' => 'LDS'],
+    ['id' => '1209', 'state_id' = '12', 'name' => 'KINABATANGAN', 'code_3' => 'KIN'],
+    ['id' => '1210', 'state_id' = '12', 'name' => 'TAWAU', 'code_3' => 'TAW'],
+    ['id' => '1211', 'state_id' = '12', 'name' => 'LAHAD DATU', 'code_3' => 'LDA'],
+    ['id' => '1212', 'state_id' = '12', 'name' => 'SEMPORNA', 'code_3' => 'SEM'],
+    ['id' => '1213', 'state_id' = '12', 'name' => 'KENINGAU', 'code_3' => 'KEN'],
+    ['id' => '1214', 'state_id' = '12', 'name' => 'TAMBUNAN', 'code_3' => 'TAM'],
+    ['id' => '1207', 'state_id' = '12', 'name' => 'PENSIANGAN', 'code_3' => 'PEN'],
+    ['id' => '1208', 'state_id' = '12', 'name' => 'TENOM', 'code_3' => 'TEN'],
+    ['id' => '1209', 'state_id' = '12', 'name' => 'BEAUFORT', 'code_3' => 'BEA'],
+    ['id' => '1210', 'state_id' = '12', 'name' => 'KUALA PENYU', 'code_3' => 'KPE'],
+    ['id' => '1211', 'state_id' = '12', 'name' => 'SIPITANG', 'code_3' => 'SIP'],
+    ['id' => '1212', 'state_id' = '12', 'name' => 'PENAMPANG', 'code_3' => 'PEN'],
+    ['id' => '1213', 'state_id' = '12', 'name' => 'KOTA MARUDU', 'code_3' => 'KMA'],
+    ['id' => '1214', 'state_id' = '12', 'name' => 'KUNAK', 'code_3' => 'KUN'],
+    ['id' => '1213', 'state_id' = '12', 'name' => 'TONGOD', 'code_3' => 'TON'],
+    ['id' => '1214', 'state_id' = '12', 'name' => 'PUTATAN', 'code_3' => 'PUT'],
+    /*
+     * sarawak
+     */
+    ['id' => '1301', 'state_id' = '13', 'name' => 'KUCHING', 'code_3' => 'KUC'],
+    ['id' => '1302', 'state_id' = '13', 'name' => 'SRI AMAN', 'code_3' => 'SAM'],
+    ['id' => '1303', 'state_id' = '13', 'name' => 'SIBU', 'code_3' => 'SIB'],
+    ['id' => '1304', 'state_id' = '13', 'name' => 'MIRI', 'code_3' => 'MIR'],
+    ['id' => '1305', 'state_id' = '13', 'name' => 'LIMBANG', 'code_3' => 'LIM'],
+    ['id' => '1306', 'state_id' = '13', 'name' => 'SARIKEI', 'code_3' => 'SAR'],
+    ['id' => '1307', 'state_id' = '13', 'name' => 'KAPIT', 'code_3' => 'KAP'],
+    ['id' => '1308', 'state_id' = '13', 'name' => 'SAMARAHAN', 'code_3' => 'SAM'],
+    ['id' => '1309', 'state_id' = '13', 'name' => 'BINTULU', 'code_3' => 'BIN'],
+    ['id' => '1310', 'state_id' = '13', 'name' => 'MUKAH', 'code_3' => 'MUK'],
+    ['id' => '1311', 'state_id' = '13', 'name' => 'BETONG', 'code_3' => 'BET']
+];
---
+++ state.php
@@ -0,0 +1,21 @@
+<?php 
+
+$items = [
+    ['name' => 'Johor', 'name_long' => 'Johor Darul Takzim', 'id' => '01', 'code2' => 'JH', 'code3' => 'JHR'],
+    ['name' => 'Kedah', 'name_long' => 'Kedah Darul Aman', 'id' => '02', 'code2' => 'KD', 'code3' => 'KDH'], 
+    ['name' => 'Kelantan', 'name_long' => 'Kelantan Darul Naim', 'id' => '03', 'code2' => 'KT', 'code3' => 'KTN'], 
+    ['name' => 'Melaka', 'name_long' => 'Melaka Bandaraya Bersejarah', 'id' => '04', 'code2' => 'ML', 'code3' => 'MLK'], 
+    ['name' => 'Negeri Sembilan', 'name_long' => ' Negeri Sembilan Darul Khusus', 'id' => '05', 'code2' => 'NS', 'code3' => 'NSN'], 
+    ['name' => 'Pahang', 'name_long' => 'Pahang Darul Makmur', 'id' => '06', 'code2' => 'PH', 'code3' => 'PHG'], 
+    ['name' => 'Pulau Pinang', 'name_long' => 'Pulau Pinang Pulau Mutiara', 'id' => '07', 'code2' => 'PN', 'code3' => 'PNG'], 
+    ['name' => 'Perak', 'name_long' => 'Perak Darul Ridzuan', 'id' => '08', 'code2' => 'PR', 'code3' => 'PRK'], 
+    ['name' => 'Perlis', 'name_long' => 'Perlis Indera Kayangan', 'id' => '09', 'code2' => 'PL', 'code3' => 'PLS'], 
+    ['name' => 'Selangor', 'name_long' => 'Selangor Darul Ehsan', 'id' => '10', 'code2' => 'SG', 'code3' => 'SGR'], 
+    ['name' => 'Terengganu', 'name_long' => 'Terengganu Darul Iman', 'id' => '11', 'code2' => 'TR', 'code3' => 'TRG'], 
+    ['name' => 'Sabah', 'name_long' => 'Sabah Negeri Di Bawah Bayu', 'id' => '12', 'code2' => 'SB', 'code3' => 'SBH'], 
+    ['name' => 'Sarawak', 'name_long' => 'Sarawak Bumi Kenyalang', 'id' => '13', 'code2' => 'SR', 'code3' => 'SRW'], 
+    ['name' => 'WP Kuala Lumpur', 'name_long' => 'Wilayah Persekutuan Kuala Lumpur', 'id' => '14', 'code2' => 'KL', 'code3' => 'WKL'], 
+    ['name' => 'WP Labuan', 'name_long' => 'Wilayah Persekutuan Labuan', 'id' => '15', 'code2' => 'LB', 'code3' => 'WPL'], 
+    ['name' => 'WP Putrajaya', 'name_long' => 'Wilayah Persekutuan Putrajaya', 'id' => '16', 'code2' => 'PJ', 'code3' => 'PJY'], 
+    ['name' => 'Luar Negara', 'name_long' => 'Luar Negara', 'id' => '98', 'code2' => 'LN', 'code3' => 'LNE']
+];
