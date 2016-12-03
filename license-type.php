d2ebdc2216139d3b42ee0937690112de40bcd27e
Muhammad Syafiq <syafiq@my-sands.com>
2016-12-03 20:53:31 +0800

    Add license type.

---
+++ license-type.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * license type
+ * @author syafiq <syafiq@my-sands.com>
+ */
+$items = [
+    [ 'id' => 1, 'name' => 'Lesen Memandu Kompeten', 'code' => 'CDL'],
+    [ 'id' => 2, 'name' => 'Lesen Memandu Percubaan', 'code' => 'PDL'],
+    [ 'id' => 3, 'name' => 'Lesen Belajar Memandu', 'code' => 'LDL'],
+    [ 'id' => 4, 'name' => 'Lesen Memandu GDL', 'code' => 'GDL'],
+    [ 'id' => 5, 'name' => 'Lesen Memandu PSV', 'code' => 'PSV'],
+    [ 'id' => 6, 'name' => 'Lesen Memandu CON', 'code' => 'CON'],
+    [ 'id' => 7, 'name' => 'Permit Memandu Antarabangsa', 'code' => 'IDP'],
+];
