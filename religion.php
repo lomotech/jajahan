8b8b3e756dc8310c63dc1a48d42895a42a0e5a3e
Muhammad Syafiq <syafiq@my-sands.com>
2016-12-03 20:53:36 +0800

    Add religion.

---
+++ religion.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Religion
+ * @author syafiq <syafiq@my-sands.com>
+ */
+$items = [
+    [ 'id' => 1, 'name' => 'Islam'],
+    [ 'id' => 2, 'name' => 'Kristian'],
+    [ 'id' => 3, 'name' => 'Buddha'],
+    [ 'id' => 4, 'name' => 'Hindu'],
+    [ 'id' => 5, 'name' => 'Sikhism'],
+    [ 'id' => 6, 'name' => 'Tao'],
+    [ 'id' => 7, 'name' => 'Konfusianisma'],
+    [ 'id' => 8, 'name' => 'Bahai'],
+    [ 'id' => 9, 'name' => 'Tiada Agama\/Aitismisme'],
+    [ 'id' => 98, 'name' => 'Lain-lain Agama'],
+    [ 'id' => 99, 'name' => 'Tiada Maklumat'],
+];
