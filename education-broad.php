f0915bfcd84faf87a4d322e4ce3276fd7c679822
Muhammad Syafiq Zainuddin <lomotech@users.noreply.github.com>
2017-05-03 04:10:14 +0800

    Update education-broad.php
    Change engineerings to engineering coz it sound so wrong.

--- education-broad.php
+++ education-broad.php
@@ -12,7 +12,7 @@ $items = [
     [ 'code' => '2', 'name' => 'ARTS AND HUMANITIES' ], 
     [ 'code' => '3', 'name' => 'SOCIAL SCIENCES, BUSINESS AND LAW' ], 
     [ 'code' => '4', 'name' => 'SCIENCES' ], // missing data from sddsa - assume
-    [ 'code' => '5', 'name' => 'ENGINEERINGS' ], // missing data from sddsa - assume
+    [ 'code' => '5', 'name' => 'ENGINEERING' ], // missing data from sddsa - assume
     [ 'code' => '6', 'name' => 'AGRICULTURE AND VETERINARY' ], 
     [ 'code' => '7', 'name' => 'HEALTH AND WELFARE' ], 
     [ 'code' => '8', 'name' => 'SERVICES' ], 
