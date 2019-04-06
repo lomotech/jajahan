fd8962e38b8730d80e15d18c3d6a5c00ada8bfc2
Muhammad Syafiq Zainuddin <tmukmkd@gmail.com>
2019-04-07 04:19:19 +0800

    Add post-office data

---
+++ php_array/post-office.php
@@ -0,0 +1,15 @@
+ <?php
+/**
+ * post office - pejabat pos.
+ */
+$items = [
+	// perlis
+	[ 'id' => 1, 'state_id' => '9' 'name' => 'Kangar' ],
+	[ 'id' => 2, 'state_id' => '9' 'name' => 'Arau' ],
+	[ 'id' => 3, 'state_id' => '9' 'name' => 'Simpang Ampat' ],
+	[ 'id' => 4, 'state_id' => '9' 'name' => 'Kuala Perlis' ],
+	[ 'id' => 5, 'state_id' => '9' 'name' => 'Padang Besar' ],
+	[ 'id' => 6, 'state_id' => '9' 'name' => 'Kaki Bukit' ],
+	//
+	
+];
